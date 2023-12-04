32adb025fdff7d4467d5d938203b576cd830160d
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-04 11:47:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import logo from './assets/images/APP BITS.png'
+
+function App() {
+  return (
+    <>
+      <img src={logo} width={300} height={200}></img>
+    </>
+  )
+}
+
+export default App
